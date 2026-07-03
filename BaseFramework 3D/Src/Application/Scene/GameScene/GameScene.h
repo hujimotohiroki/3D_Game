bfdc1442466714dc453023b6c9778a0eef8b62c9
kd1389722 <kd1389722@st.kobedenshi.ac.jp>
2026-07-03 14:44:46 +0900

    初回コミット

---
+++ BaseFramework 3D/Src/Application/Scene/GameScene/GameScene.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include"../BaseScene/BaseScene.h"
+
+class GameScene : public BaseScene
+{
+public :
+
+	GameScene()  { Init(); }
+	~GameScene() {}
+
+private:
+
+	void Event() override;
+	void Init()  override;
+
+	KdModelData m_model;
+};
